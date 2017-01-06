a6780a75ee19586512d8a65b504a1aba97bbf3b2
Jamison DeLorenzo <jdeloren@aquadev.eng.fishbowl.com>
2017-01-05 21:50:46 -0500

    src directory and content for primary pull data

---
+++ src/data/puller.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+ROOT_URL = "http://www.pro-football-reference.com/"
+TEAM_URL = "teams/"
+
+NFL_TEAM_ARRAY = [
+    'crd', 'atl', 'rav', 'buf', 'car', 'chi', 'cle', 'dal', 'den', 'det',
+    'gnb', 'htx', 'cit', 'jax', 'kan', 'ram', 'mia', 'min',
+    'nwe', 'nor', 'nyg', 'nyr', 'rai', 'phi', 'pit',
+    'sdg', 'sfo', 'sea', 'tam', 'oti', 'was']
+
+def get_data(url):
+    session = requests.session()
+    request = session.get(url)
+
+    return BeautifulSoup(request.content)
+
+def get_data_from_root(folder):
+    return get_data(ROOT_URL + folder)
+
+def get_roster_data_for_year(franchise):
+    return get_data_from_root(franchise + "/2016_roster.htm")
+
+def get_teams_static():
+    for team in NFL_TEAM_ARRAY:
+        roster = get_roster_data_for_year(TEAM_URL + team)
+        #print roster.find_all("td")
+        print roster.find_all("td", "player")
+
+def get_teams():
+    team_doc = get_data(ROOT_URL + TEAM_URL)
+
+    body = team_doc.find("tbody").find_all_next("a")
+    print body
+
+
+
+get_teams_static()
